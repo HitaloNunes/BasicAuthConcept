927f01b360f59809d0effefb4065789c436e843b
Hitalo Wingred <38959476+TioPatinhas00@users.noreply.github.com>
2022-09-05 16:36:58 -0300

    Adicionar arquivos de projeto.

---
+++ BasicAuthConcept/Handlers/BasicAuthenticationHandler.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace BasicAuthConcept.Handlers
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("Authorize header was not found!");
+            }
+
+            try
+            {
+                var authenticationHeaderValue = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var bytes = Convert.FromBase64String(authenticationHeaderValue.Parameter);
+                string[] credentials = Encoding.UTF8.GetString(bytes).Split(":");
+                if(credentials[0] == "Hitalo" && credentials[1] == "abcd.1234")
+                {
+                    var claims = new[] { new Claim(ClaimTypes.Name, credentials[0]) };
+                    var identity = new ClaimsIdentity(claims, Scheme.Name);
+                    var principal = new ClaimsPrincipal(identity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                    return AuthenticateResult.Success(ticket);
+                }
+                return AuthenticateResult.Fail("Username or Password is Invalid!");
+            } 
+            catch (Exception ex)
+            {
+                return AuthenticateResult.Fail(ex.Message);
+            }
+        }
+    }
+}
